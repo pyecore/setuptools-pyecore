910cdd4a3946fd27597f3e3e9a826c0e00b5d15f
ferraith <ferraith@gmail.com>
2018-08-24 10:53:22 +0200

    #12: Fixed default value of ecore-models option

--- tests/test_command.py
+++ tests/test_command.py
@@ -17,8 +17,8 @@ class TestPyEcoreCommand:
     def test_finalize_options_default(self, command):
         command.finalize_options()
 
-        assert command.ecore_models is None
-        assert pathlib.Path(command.output['default']) == pathlib.Path('.')
+        assert not command.ecore_models
+        assert pathlib.Path(command.output['unknown']) == pathlib.Path('.')
         assert not command.user_modules
         assert command.auto_register_package == 0
 
@@ -39,20 +39,20 @@ class TestPyEcoreCommand:
         command.output = 'default=.'
         command.finalize_options()
 
-        assert pathlib.Path(command.output['default']) == pathlib.Path('.')
+        assert pathlib.Path(command.output['unknown']) == pathlib.Path('.')
 
     def test_finalize_options_custom_output_dir(self, command):
         command.output = 'library=output/library'
         command.finalize_options()
 
-        assert pathlib.Path(command.output['default']) == pathlib.Path('.')
+        assert pathlib.Path(command.output['unknown']) == pathlib.Path('.')
         assert pathlib.Path(command.output['library']) == pathlib.Path('output/library')
 
     def test_finalize_options_custom_default_output_dir(self, command):
         command.output = 'default=output/default'
         command.finalize_options()
 
-        assert pathlib.Path(command.output['default']) == pathlib.Path('output/default')
+        assert pathlib.Path(command.output['unknown']) == pathlib.Path('output/default')
 
     def test_finalize_options_user_modules_single(self, command):
         command.user_modules = 'a=input/a.pkg.module'
